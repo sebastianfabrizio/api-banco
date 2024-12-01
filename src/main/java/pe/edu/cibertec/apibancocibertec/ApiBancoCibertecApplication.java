90a259c619b5f04335e4f0a1c0d762c9d47152d4
sebastianfabrizio <sebasotaradiaz@gmail.com>
2024-12-01 09:01:00 -0500

    initial project

---
+++ src/main/java/pe/edu/cibertec/apibancocibertec/ApiBancoCibertecApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.apibancocibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBancoCibertecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBancoCibertecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/cibertec/apibancocibertec/ApiBancoCibertecApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.apibancocibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancoCibertecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
